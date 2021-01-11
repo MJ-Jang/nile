6292a6fe8e09abdeca549387b7f295817906b9e7
mj <xkxpa@korea.ac.kr>
2021-01-11 16:46:43 +0900

    ud

--- run_finetune_kggpt2m.sh
+++ run_finetune_kggpt2m.sh
@@ -14,9 +14,9 @@ cmd="CUDA_VISIBLE_DEVICES="$GPUDEV"  python finetune_kglm.py  \
     --num_train_epochs 3 \
     --train_data_file=./dataset_snli/"$DATAROOT"/train.tsv \
     --do_eval \
-    --eval_data_file=./dataset_snli/"$DATAROOT"/dev.tsv" \
+    --eval_data_file=./dataset_snli/"$DATAROOT"/dev.tsv \
     --entity_file_path=./retrieve_knowledge/RotatE_Wn18_512d.txt \
     --num_heads=8 \
-    --entity_dim=512 \
+    --entity_dim=1000"
 echo $cmd
 eval $cmd
