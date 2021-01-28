100ac57d21840afd444ebbf9c30afbf28c0adbda
MJ-Jang <xkxpa@korea.ac.kr>
2021-01-28 03:47:38 +0000

    update

--- nli_utils.py
+++ nli_utils.py
@@ -11,8 +11,9 @@ class ExpProcessor(DataProcessor):
 
     def get_train_examples(self, filepath, data_format="instance", to_drop=[]):
         data = pd.read_csv(filepath, index_col=self.index_col)
-        # data = data.sample(frac=0.3, random_state=1234).reset_index(drop=True)
-
+        print(data[:10])
+        data = data.sample(frac=0.3, random_state=1234).reset_index(drop=True)
+        print(data[:10])
         examples = self._create_examples(data, 'train', data_format=data_format, to_drop=to_drop) 
         return examples
 
