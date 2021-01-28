f8eca0528d5edd873436b9e28fcf16c9a6431f03
MJ-Jang <xkxpa@korea.ac.kr>
2021-01-28 03:08:38 +0000

    update

--- nli_utils.py
+++ nli_utils.py
@@ -8,8 +8,11 @@ class ExpProcessor(DataProcessor):
     index_col = "pairID"
     labels = ["entailment", "contradiction", "neutral"]
     gold_label = "gold_label"
+
     def get_train_examples(self, filepath, data_format="instance", to_drop=[]):
         data = pd.read_csv(filepath, index_col=self.index_col)
+        data = data.sample(frac=0.3, random_state=1234).reset_index(drop=True)
+
         examples = self._create_examples(data, 'train', data_format=data_format, to_drop=to_drop) 
         return examples
 
